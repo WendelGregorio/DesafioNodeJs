a427f2d7cba4a0f409a5680581586c3bfd2bf3d5
Wendel Gregorio da Silva <contact.wendelg@gmail.com>
2022-04-09 16:35:03 -0300

    FixVarsGlobais

--- console.js
+++ console.js
@@ -1,3 +1,3 @@
 console.log('Hello World!')
 console.log(global) //variáveis globais
-console.log(_dirname) //diretório em que se localiza o arquivo
+console.log(__dirname) //diretório em que se localiza o arquivo
