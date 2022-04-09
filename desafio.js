01494bb09804c7c0c2dc0494cb39f2bded4f98a9
Wendel Gregorio da Silva <contact.wendelg@gmail.com>
2022-04-09 17:06:46 -0300

    DesafioFlags

---
+++ desafio.js
@@ -0,0 +1,9 @@
+/*Crie dois arquivos Javascript.
+*O primeiro irá exportar um função getFlag().
+*que receberá um argumento do tipo string.
+*Essa função devera buscar dentro do array a flag e retornar o valor da flag
+*
+*O segundo irá importar a função  e passar a flag desejada.
+*
+*Iremos roda no terminal o segundo arquivo passadom as flags --name --greeting
+*para que seja impresso no terminal a saudação e o nome da pessoa.
