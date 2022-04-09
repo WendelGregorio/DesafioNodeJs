1882ed23029cf931235d1966bb662a4a362d4aa1
Wendel Gregorio da Silva <contact.wendelg@gmail.com>
2022-04-09 16:56:27 -0300

    fixProcess

--- process.js
+++ process.js
@@ -1,5 +1,5 @@
 //console.log(process.argv)
 
 const firstName = process.argv[2]
-const lastName = process.argv[2]
+const lastName = process.argv[3]
 console.log('Seu nome é ' + firstName + ' ' + lastName)
